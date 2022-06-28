8f86c8cacec7b6ca72a47079b6257debe4e9919a
Furkan Acikgoz <furkanacikgoz1907@gmail.com>
2022-06-28 10:09:14 +0300

    Create charger app in the repo

---
+++ Charger/Charger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Charger
+//
+//  Created by Furkan Açıkgöz on 28.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
